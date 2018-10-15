7b00ca6b1415b3b5e87d5ad752a4789c0bf21f18
ph4nt0m_gh0sT <ph4nt0mgh0sT17@gmail.com>
2018-10-15 11:53:32 +0200

    Update 15_10_algorithm.cpp
    Added some new code

--- Algorithms/15_10_algorithm.cpp
+++ Algorithms/15_10_algorithm.cpp
@@ -2,11 +2,38 @@
 
 using namespace std;
 
+// function KrkoskaMain works with COPY of int 'a' not with 'a' as itself
+void krkoska_main(int a)
+{
+	a = 10;
+	cout << a << endl;
+
+	return;
+}
+
+void reference_main(int  &a)
+{
+	a = 10;
+	cout << a << endl;
+
+	return;
+}
+
+void pointer_main(int *a)
+{
+	*a = 20;
+	cout << *a << endl;
+	
+	return;
+}
+
 int main(void)
 {
 	cout << "Hello, world!" << endl << endl;
 
 	int a = 5; // initialize of integer 
+
+	// pointer has everytime same size (4 B, 8 B and so on) as the original value
 
 	int *p; // initialize of a pointer
 	p = &a; // assign address of integer a (5) to pointer
@@ -15,7 +42,41 @@ int main(void)
 	cout << "address of pointer: " << &p << endl; // address of pointer as itself
 	cout << "address of a in pointer: " << p << endl; //address of a in pointer p
 	cout << "address of a: " << &a << endl; // adress of a
-	cout << "value of a in pointer: " << *p << endl; // value of pointer (pointer reference to a)
+	cout << "value of a in pointer: " << *p << endl << endl; // value of pointer (pointer reference to a)
+	
+	// because it works only with copy
+
+	krkoska_main(a); // 10
+	cout << a << endl << endl; // 5
+
+	// now it works with parameter a itself
+	// address
+
+	reference_main(a); // 10
+	cout << a << endl << endl; // 10
+
+	// now it works with pointer
+
+	pointer_main(&a); // 20
+	cout << a << endl << endl; // 20
+
+	// we should use only reference...
+
+	int num = 5;
+	int *po = &num;
+	int **q = &po;
+	int ***o = &q;
+
+	cout << *q << endl; // address of po
+	cout << **q << endl << endl ; // value of a
+
+	cout << *o << endl;
+	cout << **o << endl;
+	cout << ***o << endl;
+	
+	
+
+	
 
 
 
