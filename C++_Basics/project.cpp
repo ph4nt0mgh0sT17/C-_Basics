b8d4ed5ef33ac4a6b9efa33e6b2b83a5fb5541b3
ph4nt0m_gh0sT <ph4nt0mgh0sT17@gmail.com>
2018-12-06 18:10:17 +0100

    Added menu and verifying input.

--- C++_Basics/project.cpp
+++ C++_Basics/project.cpp
@@ -5,6 +5,74 @@
 
 using namespace std;
 
+#define OK 0
+#define WRONG_DATA 1
+#define IO_ERROR 2
+
+
+void printTitle(string title);
+void printMenu(string menu);
+void chooseSelection(char choice);
+int verifyInput()
+{
+	string input;
+	int choice;
+	getline(cin, input);
+
+
+
+	if (input.length() > 1)
+	{
+		cout << "Musis zadat cislo v rozmezi od 1 - 5." << endl;
+	}
+	choice = stoi(input);
+
+	if (choice > 5 || choice < 1)
+	{
+		cout << "Musis zadat cislo v rozmezi od 1 - 5." << endl;
+	}
+
+	return choice;
+}
+
+
+const string nameApp = "\t\tRezervace mistnosti\n"
+					"==========================================================\n\n";
+
+const string menu = "Menu:\n"
+					"\t1 - Vypsat vsechny mistnosti\n"
+					"\t2 - Vypsat nezarezervovane mistnosti\n"
+					"\t3 - Vyhledat mistnosti do urcite ceny:\n"
+					"\t4 - Export nezarezervovanych mistnosti do HTML\n"
+					"\t5 - Ukoncit program\n\n";
+
+const string end = "Konec programu.";
+
+int main(void)
+{
+	printTitle(nameApp);
+	printMenu(menu);
+
+	int choice = verifyInput();
+
+	while (choice != 5)
+	{
+		chooseSelection(choice);
+		printMenu(menu);
+
+		choice = verifyInput();
+	}
+
+	cout << "Konec programu." << endl;
+	
+
+#ifndef __PROGTEST__
+	system("pause");
+#endif /* __PROGTEST__ */
+
+	return 0;
+}
+
 void printTitle(string title)
 {
 	for (unsigned int i = 0; i < title.length(); i++)
@@ -23,24 +91,23 @@ void printMenu(string menu)
 	}
 }
 
-const string nameApp = "\t\tRezervace mistnosti\n"
-					"==========================================================\n\n";
-
-const string menu = "Menu:\n"
-					"\t1 - Vypsat vsechny mistnosti\n"
-					"\t2 - Vypsat nezarezervovane mistnosti\n"
-					"\t3 - Vypsani vsech mistnosti\n"
-					"\t4 - Export nezarezervovane mistnosti do HTML\n"
-					"\t5 - Ukoncit program\n";
-
-int main(void)
+void chooseSelection(char choice)
 {
-	printTitle(nameApp);
-	printMenu(menu);
+	switch (choice)
+	{
+		case '1':
+			// TODO: Print all rooms from .csv file
+			break;
 
-#ifndef __PROGTEST__
-	system("pause");
-#endif /* __PROGTEST__ */
+		case '2':
+			// TODO: Print all not booked rooms
+			break;
 
-	return 0;
+		case '3':
+			// TODO: Print all rooms by the price
+			break;
+		case '4':
+			// TODO: Export rooms to HTML
+			break;
+	}
 }
