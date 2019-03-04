70b15e0ad772725618ffd6a5d9f6ac5c11f01a1f
ph4nt0m_gh0sT <ph4nt0mgh0sT17@gmail.com>
2019-03-04 11:35:22 +0100

    Update Program.cpp

--- Homeworks/PRII_3/KeyValue/Program.cpp
+++ Homeworks/PRII_3/KeyValue/Program.cpp
@@ -1,19 +1,30 @@
 #include <iostream>
-#include <string>
 
-#include "KeyValue.h"
+#include "KeyValues.h"
+
 
 using namespace std;
 
 int main()
-{
-	KeyValue** keyValues = new KeyValue*[1000];
-
-	for (int i = 0; i < 1000; i++)
+{	
+	KeyValues* kv = new KeyValues(20);
+	
+	for (int i = 0; i < 20; i++)
 	{
-		keyValues[i] = new KeyValue(i, i + 1000);
-		cout << "Key: " << to_string(keyValues[i]->GetKey()) << "; Value: " << to_string(keyValues[i]->GetValue()) << "\n";
+		kv->CreateObject(i, i + 20);
 	}
+
+	cout << kv->GetInformation();
+	
+	cout << endl << "==============================================" << endl << endl;
+
+	kv->RemoveObject(15);
+	cout << kv->GetInformation();
+
+	cout << endl << "==============================================" << endl << endl;
+
+	kv->RemoveObject(18);
+	cout << kv->GetInformation();
 
 	getchar();
 
