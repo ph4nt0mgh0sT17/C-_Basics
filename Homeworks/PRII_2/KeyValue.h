c33582221829de5e080355f28dba35b337a7b030
ph4nt0m_gh0sT <ph4nt0mgh0sT17@gmail.com>
2019-03-04 11:42:11 +0100

    Update KeyValue.h

--- Homeworks/PRII_2/KeyValue.h
+++ Homeworks/PRII_2/KeyValue.h
@@ -1,4 +1,6 @@
 #pragma once
+
+#include <string>
 class KeyValue
 {
 
@@ -14,6 +16,19 @@ public:
 	double GetValue();
 	KeyValue* GetNext();
 	KeyValue* CreateNext(int k, double v);
+	std::string GetAllKeyValues()
+	{
+		std::string result = "";
+
+		result += "Key: " + std::to_string(this->key) + "; Value: " + std::to_string(this->value) + "\n";
+
+		if (this->next != nullptr)
+		{
+			result += this->next->GetAllKeyValues();
+		}
+
+		return result;
+	}
 
 };
 
