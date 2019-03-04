90d6841aeac8dc4ab2e8b525178005df4dffee21
ph4nt0m_gh0sT <ph4nt0mgh0sT17@gmail.com>
2019-03-04 11:43:37 +0100

    Update KeyValue.cpp

--- Homeworks/PRII_2/KeyValue.cpp
+++ Homeworks/PRII_2/KeyValue.cpp
@@ -12,6 +12,11 @@ KeyValue::KeyValue(int k, double v)
 
 KeyValue::~KeyValue()
 {
+	if (this->next != nullptr)
+	{
+		delete this->next;
+		this->next = nullptr;
+	}
 }
 
 int KeyValue::GetKey()
@@ -34,4 +39,18 @@ KeyValue* KeyValue::CreateNext(int k, double v)
 	this->next = new KeyValue(k, v);
 
 	return this->next;
+}
+
+std::string KeyValue::GetAllKeyValues()
+{
+	std::string result = "";
+
+	result += "Key: " + std::to_string(this->key) + "; Value: " + std::to_string(this->value) + "\n";
+
+	if (this->next != nullptr)
+	{
+		result += this->next->GetAllKeyValues();
+	}
+
+	return result;
 }
